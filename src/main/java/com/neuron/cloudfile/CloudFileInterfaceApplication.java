e925e0192ddf6e8771fa0089194ac39e9c973d16
sunxiao3 <sunxiao3@kingsoft.com>
2022-04-15 09:25:09 +0800

    first commit

---
+++ src/main/java/com/neuron/cloudfile/CloudFileInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.neuron.cloudfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFileInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFileInterfaceApplication.class, args);
+    }
+
+}
